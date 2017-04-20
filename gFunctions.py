fad841f21f8c7c0639fb86b28137b2618f3b8740
RapidTheNerd <george.marr.cobc@gmail.com>
2017-04-20 19:51:40 +0100

    memes

--- gFunctions.py
+++ gFunctions.py
@@ -7,5 +7,14 @@ def c_p_d(d):
     c_p_d('directory_name_here')
 
 def c_d_f(p_n, b_u):
-    q = p_n + "q.txt"
-    
+    q = p_n + "/q.txt"
+    c = p_n + "/c.txt"
+    if not os.path.isfile(q):
+        w(q, b_u)
+    if not os.path.isfile(c):
+        w(c, b_u)
+
+def w(p, d):
+    f = open(p, 'w')
+    f.write(d)
+    f.close()
